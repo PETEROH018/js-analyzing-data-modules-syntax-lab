b1b4168565753585bbbf20ae9549512202e2295d
PETEROH018 <mathengep63@gmail.com>
2026-04-06 23:55:24 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
+require('datejs');
+function combineUsers(...args){
+  const combinedObject = { 
+    users : [] ,
+    merge_date : Date.today().toString("M/d/yyyy")
 
+  }
+  combinedObject.users = args.reduce((combinedArray,currentArray) => {return [...combinedArray,...currentArray]},[])
+  return combinedObject
+}
 
-module.exports = {
+module.exports = { 
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
